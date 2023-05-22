cb2936c392e5a4950cfce5b2b6d6ca9bb120ce2c
dewiz <churkin.it@gmail.com>
2023-05-23 01:08:04 +0300

    Implemented download file API

--- src/main/java/com/dewiz/springmongotest/controller/PhotoController.java
+++ src/main/java/com/dewiz/springmongotest/controller/PhotoController.java
@@ -1,11 +1,14 @@
 package com.dewiz.springmongotest.controller;
 
+import com.dewiz.springmongotest.entity.Photo;
 import com.dewiz.springmongotest.service.PhotoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -20,5 +23,15 @@ public class PhotoController {
     public String addPhoto(@RequestParam("image")MultipartFile image) throws IOException {
         String id = photoService.addPhoto(image.getOriginalFilename(), image);
         return id;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Resource> downloadPhoto(@PathVariable String id) {
+        Photo photo = photoService.getPhoto(id);
+        Resource resource = new ByteArrayResource(photo.getPhoto().getData());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + photo.getTitle() + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 }
--- src/main/java/com/dewiz/springmongotest/service/PhotoService.java
+++ src/main/java/com/dewiz/springmongotest/service/PhotoService.java
@@ -1,9 +1,12 @@
 package com.dewiz.springmongotest.service;
 
+import com.dewiz.springmongotest.entity.Photo;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface PhotoService {
     String addPhoto(String originalFilename, MultipartFile image) throws IOException;
+
+    Photo getPhoto(String id);
 }
